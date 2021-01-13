5f6aaf9ea55a2b48e9a3552aed0c3f3212892612
Taryn Martin <taryn.martin919@gmail.com>
2021-01-13 16:16:32 -0700

    Add api call and return movies

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,47 @@
+import React, { Component } from 'react';
 import Search from '../Search/Search.js';
 import Results from '../Results/Results.js';
 import Nominations from '../Nominations/Nominations.js';
+import { getMovies } from '../../apiCalls.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1 className='header-name'>The Shoppies</h1>
-      </header>
-      <Search />
-      <div className='results-nominations'>
-        <Results />
-        <Nominations />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      nominations: [],
+      searchValue: null
+    }
+  }
+
+  // searchForMovies = (searchValue, page) => {
+  //   this.setState({ searchValue: searchValue})
+  //   getMovies(process.env.REACT_APP_API_KEY, searchValue, page)
+  //     .then(data => {
+  //       console.log(data)
+  //       this.setState({ movies: data})
+  //       console.log(this.state.movies)
+  //     })
+  //     .catch(error => {
+  //       alert('Sorry, looks like we ran into some problems. Please try again.')
+  //     })
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1 className='header-name'>The Shoppies</h1>
+        </header>
+        <Search searchForMovies={this.searchForMovies}/>
+        <div className='results-nominations'>
+          <Results />
+          <Nominations />
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getMovies } from '../../apiCalls.js';
 import './Search.css';
 
-function Search() {
+function Search(props) {
+  const [searchValue, setSearchValue ] = useState('')
+
+  useEffect(() => {
+    getMovies(process.env.REACT_APP_API_KEY, searchValue, 1)
+      .then(data => {
+        console.log('data ', data)
+      })
+  }, [searchValue])
+
   return (
     <form>
       <label>Search:</label>
-      <input type='text' value='' placeholder='Rambo'></input>
-      <button>Submit</button>
+      <input type='text' value={searchValue}placeholder='Rambo' onChange={(e) => setSearchValue(e.target.value)}></input>
     </form>
   )
 }
